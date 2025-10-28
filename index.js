a61a1cd9410822288e1e31a98498052a5fdaed1b
DawrAli33285 <lemightyeagle@gmail.com>
2025-10-28 10:05:24 -0700

    new

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 const mongoose=require('mongoose');
 const adminModel = require('./admin');
-mongoose.connect('mongodb://127.0.0.1/datazappnew', {
+mongoose.connect('mongodb+srv://user:user@cluster0.pfn059x.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0', {
   serverSelectionTimeoutMS: 5000,
   socketTimeoutMS: 45000,
   family: 4
